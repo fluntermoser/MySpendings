262f7f200d3e2ae6c6b0b44e6dc665bb63d24242
Florian Untermoser <fluntermoser@edu.aau.at>
2019-01-17 16:16:11 +0100

    changed name of js file

--- server/app.js
+++ server/app.js
@@ -1,8 +1,8 @@
 var express = require('express');
-var authentication = require('./auth.js');
+var routing = require('./router.js');
 
 var app = express();
-app.use('/', authentication);
+app.use('/', routing);
 app.listen(3000, function () {
   console.log('Spendings app listening on port 3000!');
 });
